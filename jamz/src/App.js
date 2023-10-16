2883f76e4eca3efaa44381f8ad9bbe0bd631af1e
CodeConnects <codeconnects@gmail.com>
2023-10-16 16:32:28 -0400

    add tailwind styles

--- jamz/src/App.js
+++ jamz/src/App.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import './App.css';
 import MintMusicNFT from './modules/MintMusicNFT';
 
 function App() {
   return (
-    <div className="App">
+    <div className="main-app text-center">
       <h1>Jamz Music NFT Creator</h1>
       
-      <div id="creation-tools">
-        <h2>Drum Machine and Sequencer</h2>
-        <h2>Keyboard</h2>
+      <div className='flex justify-around my-10'>
+        <h2 className='flex'>Drum Machine <br />------------------<br />Sequencer</h2>
+        <h2 className='flex'>Keyboard <br />------------------<br />Recorder</h2>
       </div>
       
       <MintMusicNFT />
