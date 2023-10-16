235725006cf03a230a90cbf9d6dd19130add8361
CodeConnects <codeconnects@gmail.com>
2023-10-16 12:03:49 -0400

    add global styles

--- jamz/src/App.js
+++ jamz/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import MintMusicNFT from './modules/MintMusicNFT';
 
 function App() {
   return (
     <div className="App">
       <h1>Jamz Music NFT Creator</h1>
+      
+      <div id="creation-tools">
+        <h2>Drum Machine and Sequencer</h2>
+        <h2>Keyboard</h2>
+      </div>
+      
+      <MintMusicNFT />
     </div>
   );
 }
